77202620c51be2859531871311e1e9e92eeccf5a
Robert Maguda <robert.maguda@gmail.com>
2016-06-14 21:31:15 +0200

    Added request for events with friends

--- events-service/src/main/java/pl/edu/agh/siuu/meetings/events/EventsController.java
+++ events-service/src/main/java/pl/edu/agh/siuu/meetings/events/EventsController.java
@@ -2,6 +2,9 @@ package pl.edu.agh.siuu.meetings.events;
 
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,14 +22,14 @@ public class EventsController {
     @Autowired
     private EventsRepository eventsRepository;
 
-    @PostConstruct
+//    @PostConstruct
     public void insertFakeData() {
         List<String> participants = new ArrayList<String>();
         participants.add("Someone1");
         participants.add("Someone2");
-        participants.add("Someone3");
+        participants.add("Someone4");
         Event event = new Event(
-                "Event_1",
+                "Event_2",
                 "Some event",
                 "Category",
                 new Location(2., 1.),
@@ -42,8 +45,17 @@ public class EventsController {
         eventsRepository.insert(event);
     }
 
-    @RequestMapping(value = "/events", method = RequestMethod.GET)
-    public long test() {
-        return eventsRepository.count();
+    @RequestMapping(value = "/events/user/{id}", method = RequestMethod.GET)
+    public ResponseEntity<List<Event>> getUser(@PathVariable("id") String userId) {
+        List<String> participants = new ArrayList<String>();
+        participants.add(userId);
+        return new ResponseEntity<List<Event>>(eventsRepository.findEventsWithFriends(2,1,10,participants), HttpStatus.OK);
+
+//        Event e = eventsRepository.findOne(userId);
+//        if(user == null) {
+//            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+//        } else {
+//            return new ResponseEntity<User>(user, HttpStatus.OK);
+//        }
     }
 }
--- events-service/src/main/java/pl/edu/agh/siuu/meetings/events/EventsRepository.java
+++ events-service/src/main/java/pl/edu/agh/siuu/meetings/events/EventsRepository.java
@@ -9,8 +9,27 @@ import java.util.List;
  * Created by robert on 6/13/16.
  */
 public interface EventsRepository extends MongoRepository<Event, String>{
-    @Query("{location: {$near: [?0, ?1], $maxDistance: ?2}, participants: {$elemMatch: {category: {$in: ?3}}}, participants: {$elemMatch: {category: {$in: ?3}}}}")
-    public List<Event> findEventsWithMatchedPreferences (
+    /*
+     *    {
+     *      location:
+     *      {
+     *        $near: [?0, ?1],
+     *        $maxDistance: ?2
+     *      },
+     *      participants:
+     *      {
+     *        $elemMatch:
+     *        {
+     *          category:
+     *          {
+     *            $in: ?3
+     *          }
+     *        }
+     *      }
+     *    }
+     */
+    @Query("{location:" + "{$near: [?0, ?1], $maxDistance: ?2}, participants: {$elemMatch: {$in: ?3}}}")
+    public List<Event> findEventsWithFriends (
             double lon, double lat, double distance,
             List<String> friends
     );
